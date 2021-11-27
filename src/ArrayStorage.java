f91c93d9825fe54e7034f8cbfe4d0f4f1d97ea6b
green1eaf <aleckid77@gmail.com>
2021-11-27 20:46:40 +0300

    ArrayStorage is done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -32,6 +32,7 @@ public class ArrayStorage {
                     storage[j - 1] = storage[j];
                     if (storage[j] == null) break;
                 }
+                break;
             }
         }
     }
