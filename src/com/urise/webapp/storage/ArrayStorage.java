bf49edb1747e508fe7a7fcce08caed3cc5d11ac2
green1eaf <aleckid77@gmail.com>
2021-12-02 16:08:13 +0300

    added method indexResume. updated methods: update, save, get and delete.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -16,7 +16,7 @@ public class ArrayStorage {
         size = 0;
     }
 
-    private int indexResume(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) return i;
         }
@@ -24,7 +24,7 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        int index = indexResume(r.getUuid());
+        int index = getIndex(r.getUuid());
         if (index == -1) {
             System.out.println("ERROR: resume " + r.getUuid() + " not exists");
         } else {
@@ -33,10 +33,10 @@ public class ArrayStorage {
     }
 
     public void save(Resume r) {
-        if(size == storage.length) {
-            System.out.println("ERROR: storage is full");
-        } else if(indexResume(r.getUuid()) != -1) {
+        if (getIndex(r.getUuid()) != -1) {
             System.out.println("ERROR: resume " + r.getUuid() + " already exists");
+        } else if (size == storage.length) {
+            System.out.println("ERROR: storage overflow");
         } else {
             storage[size] = r;
             size++;
@@ -44,14 +44,14 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int index = indexResume(uuid);
+        int index = getIndex(uuid);
         if (index != -1) return storage[index];
         System.out.println("ERROR: resume " + uuid + " not exists");
         return null;
     }
 
     public void delete(String uuid) {
-        int index = indexResume(uuid);
+        int index = getIndex(uuid);
         if (index == -1) {
             System.out.println("ERROR: resume " + uuid + " not exists");
         } else {
