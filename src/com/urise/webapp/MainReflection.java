4a74663e639c36eca6aab3d1ae0c49be52e08d13
green1eaf <aleckid77@gmail.com>
2021-12-07 07:38:11 +0300

    invoke r.toString via reflection in class MainReflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,10 +3,12 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -14,6 +16,9 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         // TODO : invoke r.toString via reflection
+        System.out.println("invoke method r.toString via reflection: ");
+        Method method = r.getClass().getMethod("toString");
+        method.invoke(r);
         System.out.println(r);
     }
 }
