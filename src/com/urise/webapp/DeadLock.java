3803efb1b20c1f890f7dfd3976d77daeeb2dcce2
green1eaf <aleckid77@gmail.com>
2021-12-27 11:34:41 +0300

    lesson 11 -> Deadlock implements simple deadlock

---
+++ src/com/urise/webapp/DeadLock.java
@@ -0,0 +1,39 @@
+package com.urise.webapp;
+
+public class DeadLock {
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread1();
+        Thread thread2 = new Thread2();
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class Thread1 extends Thread {
+    public void run() {
+        System.out.println("Thread1: попытка захватить монитор lock1");
+        synchronized (DeadLock.lock1) {
+            System.out.println("Thread1: монитор lock1 захвачен");
+            System.out.println("Thread1: попытка звхватить монитор lock2");
+            synchronized (DeadLock.lock2) {
+                System.out.println("Thread1: мониторы lock1 и lock2 захвачены");
+            }
+        }
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        System.out.println("Thread2: попытка захватить монитор lock2");
+        synchronized (DeadLock.lock2) {
+            System.out.println("Thread2: монитор lock2 захвачен");
+            System.out.println("Thread2: попытка звхватить монитор lock1");
+            synchronized (DeadLock.lock1) {
+                System.out.println("Thread2: мониторы lock1 и lock2 захвачены");
+            }
+        }
+    }
+}
