08103abdbfb85a367311dd993e1ba74b7d807750
green1eaf <aleckid77@gmail.com>
2021-12-28 22:19:48 +0300

    lesson 12 -> MainStream

---
+++ src/com/urise/webapp/MainStream.java
@@ -0,0 +1,34 @@
+package com.urise.webapp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MainStream {
+
+    public static void main(String[] args) {
+        System.out.println("Method minValue");
+        int[] arr = {3, 2, 3, 2, 3, 3, 1, 2};
+        System.out.println(minValue(arr));
+
+        System.out.println("\nMethod oddOrEven");
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 16));
+        System.out.println("sum = " + list.stream().reduce(0, Integer::sum));
+        System.out.println(oddOrEven(list));
+    }
+
+    public static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (a, b) -> 10 * a + b);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().reduce(0, Integer::sum);
+        return integers.stream()
+                .filter(x -> (sum % 2 == 0) == (x % 2 != 0))
+                .collect(Collectors.toList());
+    }
+}
