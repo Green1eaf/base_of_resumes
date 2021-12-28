10f7213608f9325c488ace431b58199464daaa8a
green1eaf <aleckid77@gmail.com>
2021-12-28 14:40:02 +0300

    lesson 11 -> Deadlock implements simple deadlock IS DONE

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -11,17 +11,18 @@ public class DeadLock {
 
     public static void deadLock(String lock1, String lock2) {
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": попытка захватить монитор " + lock1);
+            String name = Thread.currentThread().getName();
+            System.out.println(name + ": попытка захватить монитор " + lock1);
             synchronized (lock1) {
-                System.out.println(Thread.currentThread().getName() + ": монитор " + lock1 + " захвачен");
+                System.out.println(name + ": монитор " + lock1 + " захвачен");
                 try {
                     Thread.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(Thread.currentThread().getName() + ": попытка захватить монитор " + lock2);
+                System.out.println(name + ": попытка захватить монитор " + lock2);
                 synchronized (lock2) {
-                    System.out.println(Thread.currentThread().getName() + ": мониторы " + lock1 + " и " + lock2 + " захвачены");
+                    System.out.println(name + ": мониторы " + lock1 + " и " + lock2 + " захвачены");
                 }
             }
         }).start();
