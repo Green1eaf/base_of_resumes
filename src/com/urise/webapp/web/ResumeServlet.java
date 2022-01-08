60c6d89a495f08aa45014219ee822fd362607bae
olegs <aleckid77@gmail.com>
2022-01-08 23:53:47 +0300

    lesson 15 -> HW ResumeServlet can't initialize

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,11 +1,18 @@
 package com.urise.webapp.web;
 
+import com.urise.webapp.Config;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.Storage;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResumeServlet extends HttpServlet {
+    private final Storage storage = Config.get().getStorage();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
@@ -13,9 +20,23 @@ public class ResumeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        PrintWriter writer = response.getWriter();
+
+        writer.println("<table>\n" +
+                "<tbody>\n" +
+                "<tr>\n" +
+                "<th>UUID</th>\n" +
+                "<th>Full Name</th>\n" +
+                "</tr>");
+
+        for (Resume r : storage.getAllSorted()) {
+            writer.println("<tr>");
+            writer.println("<td>" + r.getUuid() + "</td>");
+            writer.println("<td>" + r.getFullName() + "</td>");
+            writer.println("</tr>");
+        }
+
+        writer.println("</tbody\n" +
+                "</table>");
     }
 }
